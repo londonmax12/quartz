b4fc0298d45c0e05ff94dc4e53362b810b6117b5
London Ball <56260362+londonmax12@users.noreply.github.com>
2023-09-17 13:47:12 +1000

    Initial commit

---
+++ build/CMakeFiles/quartz-compiler.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quartz-compiler.dir/quartz-compiler/src/main.cpp.o"
+  "CMakeFiles/quartz-compiler.dir/quartz-compiler/src/main.cpp.o.d"
+  "quartz-compiler"
+  "quartz-compiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quartz-compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
